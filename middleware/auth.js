04719d6bbf83a80421478f4d4df84987369ca496
Adrian <mailforadrian@me.com>
2020-04-17 01:42:46 +0200

    added auth middleware. protected getLoggedInUser route with auth middleware.

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+// this middleware will be used on all routes that shall not be publicly available and require authentification
+
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+// export this function
+module.exports = function (req, res, next) {
+  // get token from header by using the key
+  const token = req.header("x-auth-token");
+
+  // check if there is no token
+  if (!token) {
+    return res.status(401).json({ msg: "No token. authentication denied." });
+  }
+
+  try {
+    // after verification, the payload gets put into the variable "decoded"
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+    // retrieve the user object from the payload
+    // make the "user" accessible inside of the route by saving the decoded user info in the req.user object
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid." });
+  }
+};
