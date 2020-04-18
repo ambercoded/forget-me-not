02d57e90bcae057d30425cd35f90cdb51c2c9867
Adrian <mailforadrian@me.com>
2020-04-18 16:57:27 +0200

    added navbar and routing for home and about.

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className="my-1">
+        This is a full stack app for sharing tasks with your loved one. And
+        getting rewarded for fulfilling those tasks.
+      </p>
+    </div>
+  );
+};
+
+export default About;
